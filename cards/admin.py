fa8a208e56a2176590d3d8672bdf6283740b7af2
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-02-13 12:36:31 +0100

    Add admin panel for models

--- cards/admin.py
+++ cards/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+
+class CardInlineModel(admin.TabularInline):
+    model = models.Card
+    fields = [
+        'content',
+        'type',
+    ]
+
+
+@admin.register(models.Deck)
+
+class DeckAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+    ]
+
+    inlines = [
+        CardInlineModel,
+    ]
+
+@admin.register(models.Card)
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = [
+        'content'
+    ]
