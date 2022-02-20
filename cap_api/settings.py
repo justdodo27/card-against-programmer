c15b0103c93692c131cffe63307e91e70ce673f5
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-02-20 17:01:09 +0100

    Add game application

--- cap_api/settings.py
+++ cap_api/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'cards',
+    'game',
     'oauth2_provider',
     'rest_framework'
 ]
@@ -107,7 +108,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'var/static_root/'
+STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- cap_api/urls.py
+++ cap_api/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cards.urls', namespace='card')),
+    path('game/', include('game.urls', namespace='game')),
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
