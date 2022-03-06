09c7285640e2ab0eb8c0f717034d8f5c0bae00eb
Dominik Sawicki <dominik.sawicki52@gmail.com>
2022-03-06 15:52:04 +0100

    Update gmae password authentication

---
+++ vueapp/src/components/PasswordForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <form @submit.prevent="">
+        <div class="input-group">
+            <label for="#password-input">Password</label>
+            <input id="password-input" type="text" v-model="password">
+        </div>
+        <hr>
+        <button @click="sendPassword()">Send Password</button>
+    </form>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    data(){
+        return {
+            password: ""
+        }
+    },
+    methods: {
+        sendPassword(){
+            this.$emit('submitForm', {password: this.password})
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .input-group{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 0 20px;
+    }
+</style>
